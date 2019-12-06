8f73f7e391638d4e70d3857abdc4010075c06306
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-06 15:25:22 +1300

    building

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from django.contrib.auth.model import User
+from django.contrib.auth.models import User
 # Create your models here.
 # One Class => one table
 # All tables(classes) must inherit from models.Model
 # id will be created by Django automatically
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -26,5 +30,6 @@ class Post(models.Model):
 
     #User is imported from django.contrib.auth.models
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.name
 
