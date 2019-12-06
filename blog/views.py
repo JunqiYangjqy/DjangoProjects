d7fe306b8e7890c66bc913c2961261a2fd2cd5cc
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-06 16:59:50 +1300

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Welcome to this page")
-    return render(request,'blog/index.html',context={
-        'title':'Homepage of My Blog',
-        'welcome':'Welcome to My Blog'
-    })
+    # return render(request,'blog/index.html',context={
+    #     'title':'Homepage of My Blog',
+    #     'welcome':'Welcome to My Blog'
+    # })
+
+    # '-' means 'reverse=True'
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list':post_list})
