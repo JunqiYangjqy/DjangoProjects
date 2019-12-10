f4f9b4e5102aec01cc7acae607e0a3a67800e472
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-10 16:10:29 +1300

    changed the display name in admin

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,19 @@ from django.contrib.auth.models import User
 # id will be created by Django automatically
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+    
     def __str__(self):
         return self.name
 
@@ -28,8 +36,14 @@ class Post(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag,blank=True)
 
-    #User is imported from django.contrib.auth.models
+    # User is imported from django.contrib.auth.models
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    # Use internal class Meta to configurate model
+    class Meta:
+        verbose_name = 'Post-Article'
+        verbose_name_plural = "Post-Articles"
+    
     def __str__(self):
         return self.title
 
