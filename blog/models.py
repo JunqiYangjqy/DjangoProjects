af6680b0efbd596915e616773b019345a93865ba
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-10 14:38:22 +1300

    minor problems fixed

--- blog/models.py
+++ blog/models.py
@@ -31,5 +31,5 @@ class Post(models.Model):
     #User is imported from django.contrib.auth.models
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.title
 
