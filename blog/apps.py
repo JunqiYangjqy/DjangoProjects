5aa6841652e129a0ffdf1a66a08f0d185caaab67
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-10 16:10:52 +1300

    change the registered app's display name in admin

--- blog/apps.py
+++ blog/apps.py
@@ -1,5 +1,9 @@
+# Automatically generated code when executing startapp command
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
     name = 'blog'
+    # To modify the name displayed in admin, use verbose_name
+    verbose_name = "Blog"
+    # Then we need to register this in settings.py
