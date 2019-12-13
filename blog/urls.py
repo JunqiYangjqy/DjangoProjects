5ed82a4a66da79b6d02e7710904c2af956d5c66f
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-13 15:12:09 +1300

    added new path rule

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 
+# Assign namespace to avoid NoMatchReserved error
+app_name = 'blog'
+
+# Design RESTful URL
 urlpatterns = [
     path('',views.index,name='index')
+    # <int:pk> is django URL routing match rules
+    # which means capture all the int numbers from user request
+    # and the transit it to 'pk', then to 'detail' (view) function
+    # Calling detail function resembles: 
+    # detail(request, pk=255)
+    path('post/<int:pk>',views.detail,name=detail),
+
 ]
