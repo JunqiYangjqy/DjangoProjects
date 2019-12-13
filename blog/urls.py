32d958af23f2f10fe92d355c4b123a6c1958b0a9
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-12-13 16:53:29 +1300

    problem fixed

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,14 @@ from . import views
 app_name = 'blog'
 
 # Design RESTful URL
+"""
+    int:pk is django URL routing match rules
+    which means capture all the int numbers from user request
+    and the transit it to 'pk', then to 'detail' (view) function
+    Calling detail function resembles: 
+    detail(request, pk=255)
+"""
 urlpatterns = [
-    path('',views.index,name='index')
-    # <int:pk> is django URL routing match rules
-    # which means capture all the int numbers from user request
-    # and the transit it to 'pk', then to 'detail' (view) function
-    # Calling detail function resembles: 
-    # detail(request, pk=255)
-    path('post/<int:pk>',views.detail,name=detail),
-
+    path('',views.index,name='index'),
+    path('posts/<int:pk>',views.detail,name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
 def index(request):
-    #return HttpResponse("Welcome to this page")
-    # return render(request,'blog/index.html',context={
-    #     'title':'Homepage of My Blog',
-    #     'welcome':'Welcome to My Blog'
-    # })
+    """
+    return HttpResponse("Welcome to this page")
+    return render(request,'blog/index.html',context={
+        'title':'Homepage of My Blog',
+        'welcome':'Welcome to My Blog'
+    }) 
+    """
 
     # '-' means 'reverse=True'
     post_list = Post.objects.all().order_by('-created_time')
     return render(request,'blog/index.html',context={'post_list':post_list})
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    
+    """
+    视图函数很简单，
+    它根据我们从 URL 捕获的文章 id（
+    也就是 pk，这里 pk 和 id 是等价的）
+    获取数据库中文章 id 为该值的记录，然后传递给模板(Model)
+    post.get_absolute_url 最终会被替换成该 post 自身的 URL。
+    This function will match the id we got from URL(aka pk) with the 
+    id in database, and forward to model
+    """
+
+    # get_object_or_404() will return 404 error if requested resources 
+    # (in this situation is article) not exist.
+    return render(request,'blog/detail.html',context={'post': post})
